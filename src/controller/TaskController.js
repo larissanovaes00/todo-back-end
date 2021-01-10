df8246e0162fdeeb3078499c9cf73de88e780ad5
Larissa Novaes <larissanovaes00@hotmail.com>
2021-01-10 18:27:22 -0300

    inserindo função para buscar tarefa específica

--- src/controller/TaskController.js
+++ src/controller/TaskController.js
@@ -1,47 +1,61 @@
-const TaskModel = require('../model/TaskModel');
+const TaskModel = require("../model/TaskModel");
 
 // feito em modelo de classe para poder utilizar os métodos de maneira componentizada,
 // posso utilizar os métodos criados separadamente dessa forma
 
 class TaskController {
-
   async create(req, res) {
     // pega o body da task criada e configura de acordo com meu TaskModel
     const task = new TaskModel(req.body);
     // espera minha task ser salva e verifica se deu tudo certo
-    await task.save()
-    .then(response => {
-      return res.status(200).json(response);
-    })
-    .catch(error => {
-      return res.status(500).json(error);
-    });
+    await task
+      .save()
+      .then((response) => {
+        return res.status(200).json(response);
+      })
+      .catch((error) => {
+        return res.status(500).json(error);
+      });
   }
 
   async update(req, res) {
-    await TaskModel.findByIdAndUpdate({'_id': req.params.id}, req.body, {new: true})
-    .then(response => {
-      return res.status(200).json(response);
+    await TaskModel.findByIdAndUpdate({ _id: req.params.id }, req.body, {
+      new: true,
     })
-    .catch(error => {
-      return res.status(500).json(error);
-    });
+      .then((response) => {
+        return res.status(200).json(response);
+      })
+      .catch((error) => {
+        return res.status(500).json(error);
+      });
   }
 
-  async all(req, res){
+  async all(req, res) {
     await TaskModel.find({
-      macaddress:{'$in': req.body.macaddress}
+      macaddress: { $in: req.body.macaddress },
     })
-    .sort('when')
-    .then(response => {
-      return res.status(200).json(response);
+      .sort("when")
+      .then((response) => {
+        return res.status(200).json(response);
+      })
+      .catch((error) => {
+        return res.status(500).json(error);
+      });
+  }
+
+  async show(req, res) {
+    await TaskModel.findById(req.params.id)
+    .then((response) => {
+      if (response) {
+        return res.status(200).json(response);
+      } else {
+        return res.status(404).json({ error: "tarefa não encontrada" });
+      }
     })
-    .catch(error => {
+    .catch((error) => {
       return res.status(500).json(error);
     });
   }
-
-
 }
 
 module.exports = new TaskController();
--- src/routes/TaskRoutes.js
+++ src/routes/TaskRoutes.js
@@ -7,6 +7,7 @@ const MacaddressValidation = require('../middlewares/MacaddressValidation');
 // funções só são executadas depois que os campos requeridos forem validados
 router.post('/', TaskValidation, TaskController.create);
 router.put('/:id', TaskValidation, TaskController.update);
+router.get('/:id', TaskController.show);
 
 router.get('/filter/all', MacaddressValidation, TaskController.all);
 
